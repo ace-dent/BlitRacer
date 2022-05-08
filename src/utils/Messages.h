905c04f33a44edbf76fa9a48decb35b0786ed959
Andrew Dent <dent.ace@gmail.com>
2022-05-08 17:55:13 +0100

    Tweak `Messages.h`
    Improvements thanks to @Pharap.

--- src/utils/Messages.h
+++ src/utils/Messages.h
@@ -6,58 +6,56 @@
 constexpr bool newLine = true;
 
 
-// Print a string in program memory to the Arduboy screen
+// Print a string from program memory to the Arduboy screen
 void logArduboy(const char* str, bool newline = false) {
     arduboy.setCursor(2,8);
     arduboy.print((__FlashStringHelper*)(str));
-    if (newline) arduboy.print("\n");
+    if (newline) arduboy.print(F("\n"));
     arduboy.display();
     delay(1000);
 }
 
-// Print a string in program memory to the serial console
+// Print a string from program memory to the serial console
 void logConsole(const char* str, bool newline = false) {
     Serial.print((__FlashStringHelper*)(str));
-    if (newline) Serial.print("\n");
+    if (newline) Serial.print(F("\n"));
     delay(50);
 }
 
 
 // Message text
-const char PROGMEM msgSetup[]               = "Setup complete...\n";
+constexpr char PROGMEM msgSetup[]               = "Setup complete...\n";
 
-const char PROGMEM msgBaselineTest[]        = "* CPU baseline test *\n";
-const char PROGMEM msgFCPU[]                = "F_CPU = ";
-const char PROGMEM msgHertz[]               = " Hz\n";
-const char PROGMEM msgOneCycle[]            = "1 cycle             : ";
-const char PROGMEM msgNop[]                 = "nop                 : ";
-const char PROGMEM msgLoopOverhead[]        = "Loop overhead       : ";
+constexpr char PROGMEM msgBaselineTest[]        = "* CPU baseline test *\n";
+constexpr char PROGMEM msgFCPU[]                = "F_CPU = ";
+constexpr char PROGMEM msgHertz[]               = " Hz\n";
+constexpr char PROGMEM msgOneCycle[]            = "1 cycle             : ";
+constexpr char PROGMEM msgNop[]                 = "nop                 : ";
+constexpr char PROGMEM msgLoopOverhead[]        = "Loop overhead       : ";
 
-const char PROGMEM msgMonopixTest[]         = "* Monopixel test    *\n";
-
-const char PROGMEM msgHeroTest[]            = "* 8x8 'Hero' test   *\n";
-
-const char PROGMEM msgBannerTest[]          = "* Banner test       *\n";
+constexpr char PROGMEM msgMonopixTest[]         = "* Monopixel test    *\n";
+constexpr char PROGMEM msgHeroTest[]            = "* 8x8 'Hero' test   *\n";
+constexpr char PROGMEM msgBannerTest[]          = "* Banner test       *\n";
 
 
 // Common text
-const char PROGMEM msgBlank[]               = "";
-const char PROGMEM msgTitleBreak[]          = "*********************\n\n";
-const char PROGMEM msgHorizontalRule[]      = "---------------------\n\n";
-const char PROGMEM msgMicros[]              = " µs\n";
-const char PROGMEM msgTestAlign[]           = "   - Byte aligned\n";
-const char PROGMEM msgTestUnalign[]         = "   - Byte unaligned\n";
+constexpr char PROGMEM msgBlank[]               = "";
+constexpr char PROGMEM msgTitleBreak[]          = "*********************\n\n";
+constexpr char PROGMEM msgHorizontalRule[]      = "---------------------\n\n";
+constexpr char PROGMEM msgMicros[]              = " µs\n";
+constexpr char PROGMEM msgTestAlign[]           = "   - Byte aligned\n";
+constexpr char PROGMEM msgTestUnalign[]         = "   - Byte unaligned\n";
 
-const char PROGMEM msgSpritesOverwrite[]    = "Sprites Overwrite   : ";
-const char PROGMEM msgSpritesErase[]        = "Sprites Erase       : ";
-const char PROGMEM msgSpritesSelfMask[]     = "Sprites Self masked : ";
-const char PROGMEM msgSpritesEraseMasked[]  = "Sprites Self w/mask : ";
-const char PROGMEM msgSpritesExtMask[]      = "Sprites Ext' mask   : ";
-const char PROGMEM msgSpritesPlusMask[]     = "Sprites Plus mask   : ";
+constexpr char PROGMEM msgSpritesOverwrite[]    = "Sprites Overwrite   : ";
+constexpr char PROGMEM msgSpritesErase[]        = "Sprites Erase       : ";
+constexpr char PROGMEM msgSpritesSelfMask[]     = "Sprites Self masked : ";
+constexpr char PROGMEM msgSpritesEraseMasked[]  = "Sprites Self w/mask : ";
+constexpr char PROGMEM msgSpritesExtMask[]      = "Sprites Ext' mask   : ";
+constexpr char PROGMEM msgSpritesPlusMask[]     = "Sprites Plus mask   : ";
 
-const char PROGMEM msgBitmapsStd[]          = "Bitmaps Standard    : ";
-const char PROGMEM msgBitmapsSlow[]         = "Bitmaps Slow XY     : ";
-const char PROGMEM msgBitmapsComp[]         = "Bitmaps Compressed  : ";
-const char PROGMEM msgBitmapsStdMasked[]    = "Bitmaps Std w/mask  : ";
-const char PROGMEM msgBitmapsSlowMasked[]   = "Bitmaps Slow w/mask : ";
-const char PROGMEM msgBitmapsCompMasked[]   = "Bitmaps Comp w/mask : ";
+constexpr char PROGMEM msgBitmapsStd[]          = "Bitmaps Standard    : ";
+constexpr char PROGMEM msgBitmapsSlow[]         = "Bitmaps Slow XY     : ";
+constexpr char PROGMEM msgBitmapsComp[]         = "Bitmaps Compressed  : ";
+constexpr char PROGMEM msgBitmapsStdMasked[]    = "Bitmaps Std w/mask  : ";
+constexpr char PROGMEM msgBitmapsSlowMasked[]   = "Bitmaps Slow w/mask : ";
+constexpr char PROGMEM msgBitmapsCompMasked[]   = "Bitmaps Comp w/mask : ";
