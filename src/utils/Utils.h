1f54ab388f8b3d8ef86c48972ce943d74ad0819f
Andrew Dent <dent.ace@gmail.com>
2022-05-09 21:58:40 +0100

    Make `benchmark()` rendering optional

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -5,13 +5,18 @@ uint32_t benchStartTime, benchEndTime; // Unsigned long for millis()
 float    benchResult, benchAverage, benchOverhead;
 
 
-void benchmark(uint16_t loops) {
+
+void benchmark(uint16_t loops, bool render = true) {
 
     benchResult = ( (float)benchEndTime - (float)benchStartTime ) / ( (float)loops ); // Calculate benchmark time for an individual loop
     benchResult = benchResult * (float)1000; // Convert milli (10^-3) to micro (10^-6) seconds
     benchResult = benchResult - benchOverhead; // Remove overhead of 1 loop and loop counter, from results
 
+    benchAverage = benchAverage + benchResult;
+
     // Bonus: render the screen buffer
-    arduboy.display(CLEAR_BUFFER);
-    delay(200);
+    if (render) {
+        arduboy.display(CLEAR_BUFFER);
+        delay(200);
+    }
 }
